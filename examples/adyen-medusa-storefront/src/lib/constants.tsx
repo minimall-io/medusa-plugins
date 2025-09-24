ec8ed36e2a276e55fc8711894bd8566f5c386100
minimall-dev <dev@minimall.io>
2025-09-24 00:31:49 +0000

    Extend constants library to support Adyen

--- examples/adyen-medusa-storefront/src/lib/constants.tsx
+++ examples/adyen-medusa-storefront/src/lib/constants.tsx
@@ -1,8 +1,8 @@
-import React from "react"
 import { CreditCard } from "@medusajs/icons"
+import React from "react"
 
-import Ideal from "@modules/common/icons/ideal"
 import Bancontact from "@modules/common/icons/bancontact"
+import Ideal from "@modules/common/icons/ideal"
 import PayPal from "@modules/common/icons/paypal"
 
 /* Map of payment provider_id to their title and icon. Add in any payment providers you want to use. */
@@ -10,16 +10,20 @@ export const paymentInfoMap: Record<
   string,
   { title: string; icon: React.JSX.Element }
 > = {
+  pp_adyen_MinimallLLCECOM: {
+    title: "Credit Card (Adyen)",
+    icon: <CreditCard />,
+  },
   pp_stripe_stripe: {
-    title: "Credit card",
+    title: "Credit Card (Stripe)",
     icon: <CreditCard />,
   },
   "pp_stripe-ideal_stripe": {
-    title: "iDeal",
+    title: "iDeal (Stripe)",
     icon: <Ideal />,
   },
   "pp_stripe-bancontact_stripe": {
-    title: "Bancontact",
+    title: "Bancontact (Stripe)",
     icon: <Bancontact />,
   },
   pp_paypal_paypal: {
@@ -34,15 +38,25 @@ export const paymentInfoMap: Record<
 }
 
 // This only checks if it is native stripe for card payments, it ignores the other stripe-based providers
-export const isStripe = (providerId?: string) => {
-  return providerId?.startsWith("pp_stripe_")
-}
-export const isPaypal = (providerId?: string) => {
-  return providerId?.startsWith("pp_paypal")
-}
-export const isManual = (providerId?: string) => {
-  return providerId?.startsWith("pp_system_default")
-}
+export const isDefined = (providerId?: string) => providerId !== undefined
+
+export const isAdyen = (providerId?: string) =>
+  isDefined(providerId) && providerId.startsWith("pp_adyen_")
+
+export const isStripe = (providerId?: string) =>
+  isDefined(providerId) && providerId.startsWith("pp_stripe_")
+
+export const isPaypal = (providerId?: string) =>
+  isDefined(providerId) && providerId.startsWith("pp_paypal")
+
+export const isManual = (providerId?: string) =>
+  isDefined(providerId) && providerId.startsWith("pp_system_default")
+
+export const isUnknown = (providerId?: string) =>
+  !isAdyen(providerId) &&
+  !isStripe(providerId) &&
+  !isPaypal(providerId) &&
+  !isManual(providerId)
 
 // Add currencies that don't need to be divided by 100
 export const noDivisionCurrencies = [
