82cc99be12b9cb335124c9bffb66a171a131324c
minimall-dev <dev@minimall.io>
2025-10-09 08:31:02 +0000

    Switch to the Adyen Dropin component.

--- examples/adyen-medusa-storefront/src/modules/checkout/components/payment-providers/adyen-provider.tsx
+++ examples/adyen-medusa-storefront/src/modules/checkout/components/payment-providers/adyen-provider.tsx
@@ -1,57 +1,65 @@
-import { AdyenCheckout, Card, Core } from "@adyen/adyen-web"
-import { AdyenEnvironment, IAdyenPayment } from "@modules/checkout/hooks"
-import { useCallback, useEffect, useRef, useState } from "react"
+import {
+  AdyenCheckout,
+  Card,
+  CardConfiguration,
+  Core,
+  Dropin,
+  DropinConfiguration,
+  Klarna,
+} from "@adyen/adyen-web"
+import "@adyen/adyen-web/styles/adyen.css"
+import { IAdyenPayment } from "@modules/checkout/hooks"
+import { useEffect, useRef, useState } from "react"
 
 interface Props {
   payment: IAdyenPayment
 }
 
-const clientKey = process.env.NEXT_PUBLIC_ADYEN_CLIENT_KEY
-const environment = (process.env.NEXT_PUBLIC_ADYEN_ENVIRONMENT ||
-  "test") as AdyenEnvironment
-
-const cardConfiguration = {
+const card: CardConfiguration = {
   enableStoreDetails: true,
 }
 
+const dropinConfiguration: DropinConfiguration = {
+  showStoredPaymentMethods: true,
+  showPaymentMethods: true,
+  showRemovePaymentMethodButton: true,
+  showRadioButton: true,
+  paymentMethodComponents: [Card, Klarna],
+  paymentMethodsConfiguration: {
+    card,
+  },
+}
+
 const AdyenProviderOption = ({ payment }: Props) => {
-  const newCardRef = useRef<HTMLDivElement>(null)
   const [checkout, setCheckout] = useState<Core | null>(null)
+  const containerRef = useRef<HTMLDivElement>(null)
+  const { config } = payment
+  const { countryCode, paymentMethodsResponse, clientKey } = config
 
-  const { countryCode, onChange, onError } = payment.config
+  console.log("AdyenProviderOption/config:", config)
 
-  const onInit = useCallback(async () => {
-    if (!clientKey || !countryCode) return
-    try {
-      const config = {
-        environment,
-        clientKey,
-        countryCode,
-        showPayButton: false,
-        onChange,
-        onError,
+  useEffect(() => {
+    const initateCheckout = async () => {
+      try {
+        const checkout = await AdyenCheckout(config)
+        setCheckout(checkout)
+      } catch (error) {
+        setCheckout(null)
+        console.error("Error initializing Adyen checkout configuration:", error)
       }
-      const checkout = await AdyenCheckout(config)
-      setCheckout(checkout)
-    } catch (error) {
-      setCheckout(null)
-      console.error("Error initializing Adyen checkout configuration:", error)
     }
-  }, [countryCode])
+    if (clientKey && countryCode) initateCheckout()
+  }, [countryCode, paymentMethodsResponse])
 
   useEffect(() => {
-    if (clientKey && countryCode) onInit()
-  }, [countryCode])
-
-  useEffect(() => {
-    if (!checkout || !newCardRef.current) return
+    if (!checkout || !containerRef.current) return
 
     try {
-      const newCard = new Card(checkout, cardConfiguration)
-      newCard.mount(newCardRef.current)
+      const dropin = new Dropin(checkout, dropinConfiguration)
+      dropin.mount(containerRef.current)
 
       return () => {
-        newCard.unmount()
+        dropin.unmount()
       }
     } catch (error) {
       console.error("Error mounting Adyen CustomCard:", error)
@@ -60,7 +68,7 @@ const AdyenProviderOption = ({ payment }: Props) => {
 
   if (!checkout) return null
 
-  return <div id="new-card" ref={newCardRef}></div>
+  return <div id="adyen-container" ref={containerRef}></div>
 }
 
 export default AdyenProviderOption
