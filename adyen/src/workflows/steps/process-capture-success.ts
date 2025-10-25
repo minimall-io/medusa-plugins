d770396ee4848c6eb52738820477bdaeba4e123c
minimall-dev <dev@minimall.io>
2025-10-25 04:09:30 +0000

    Update process capture sucess step to manage captures

--- adyen/src/workflows/steps/process-capture-success.ts
+++ adyen/src/workflows/steps/process-capture-success.ts
@@ -20,7 +20,7 @@ const processCaptureSuccess = (
   notification: NotificationRequestItem,
   payment: PaymentDTO,
 ): PaymentDTO => {
-  const { pspReference, success } = notification
+  const { pspReference, success, merchantReference } = notification
   const { data } = payment
   const status = success === SuccessEnum.True ? 'success' : 'failed'
 
@@ -38,7 +38,9 @@ const processCaptureSuccess = (
     return { id: payment.id, data: newData } as PaymentDTO
   }
 
-  const paymentCaptureRequests = { [pspReference]: { ...notification, status } }
+  const paymentCaptureRequests = {
+    [pspReference]: { ...notification, status, reference: merchantReference },
+  }
   const newData = { ...data, paymentCaptureRequests } as PaymentDTO['data']
   return { id: payment.id, data: newData } as PaymentDTO
 }
@@ -49,12 +51,28 @@ const processCaptureSuccessStepInvoke = async (
 ): Promise<StepResponse<PaymentDTO, PaymentDTO>> => {
   const { merchantReference, amount } = notification
   const paymentService = container.resolve(Modules.PAYMENT)
-  const [originalPayment] = await paymentService.listPayments({
-    payment_session_id: merchantReference,
-  })
+  const [originalPayment] = await paymentService.listPayments(
+    {
+      payment_session_id: merchantReference,
+    },
+    {
+      relations: ['captures'],
+    },
+  )
 
-  const newPayment = processCaptureSuccess(notification, originalPayment)
-  const updatedPayment = await paymentService.updatePayment(newPayment)
+  console.log(
+    'processCaptureSuccessStepInvoke/originalPayment',
+    JSON.stringify(originalPayment, null, 2),
+  )
+
+  const processedPayment = processCaptureSuccess(notification, originalPayment)
+
+  console.log(
+    'processCaptureSuccessStepInvoke/processedPayment',
+    JSON.stringify(processedPayment, null, 2),
+  )
+
+  const updatedPayment = await paymentService.updatePayment(processedPayment)
 
   if (
     updatedPayment.data?.paymentCaptureRequests &&
@@ -66,24 +84,59 @@ const processCaptureSuccessStepInvoke = async (
       amount: getAmountFromMinorUnit(amount.value, amount.currency),
       captured_by: notification.merchantAccountCode,
     }
-    const capturedPayment = await paymentService.capturePayment(capture)
-    return new StepResponse<PaymentDTO, PaymentDTO>(
-      capturedPayment,
-      originalPayment,
-    )
+    await paymentService.capturePayment(capture)
   }
 
-  return new StepResponse<PaymentDTO, PaymentDTO>(
-    updatedPayment,
-    originalPayment,
+  const newPayment = await paymentService.retrievePayment(originalPayment.id, {
+    relations: ['captures'],
+  })
+
+  console.log(
+    'processCaptureSuccessStepInvoke/newPayment',
+    JSON.stringify(newPayment, null, 2),
   )
+
+  return new StepResponse<PaymentDTO, PaymentDTO>(newPayment, originalPayment)
 }
+
+/**
+ *
+ * "captures": [
+    {
+      "id": "capt_01K8CPEEE5B7FP9Y0PXVH2TNST",
+      "payment_id": "pay_01K8CP9CS4HSZB7PMG3T3YMCDF",
+      "metadata": null,
+      "created_by": null,
+      "raw_amount": {
+        "value": "25",
+        "precision": 20
+      },
+      "created_at": "2025-10-25T03:26:26.757Z",
+      "updated_at": "2025-10-25T03:26:26.757Z",
+      "deleted_at": null,
+      "amount": 25
+    }
+  ],
+ */
 
 const processCaptureSuccessStepCompensate = async (
   originalPayment: PaymentDTO,
   { container }: StepExecutionContext,
 ): Promise<StepResponse<PaymentDTO>> => {
   const paymentService = container.resolve(Modules.PAYMENT)
+  const newPayment = await paymentService.retrievePayment(originalPayment.id, {
+    relations: ['captures'],
+  })
+  const originalCaptures = originalPayment.captures || []
+  const newCaptures = newPayment.captures || []
+  const originalCaptureIds = new Set(
+    originalCaptures.map<string>((capture) => capture.id),
+  )
+  const newCaptureIds = newCaptures.map<string>((capture) => capture.id)
+  const capturesToDelete = newCaptureIds.filter(
+    (id) => !originalCaptureIds.has(id),
+  )
+  await paymentService.deleteCaptures(capturesToDelete)
   const updatedPayment = await paymentService.updatePayment(originalPayment)
   return new StepResponse<PaymentDTO>(updatedPayment)
 }
