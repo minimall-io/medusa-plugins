07cf55d3d21677329d1fc2057d8e104c9be15a03
minimall-dev <dev@minimall.io>
2025-11-06 18:09:25 +0000

    Finish resetting the package.json and related dependencies

---
+++ adyen/jest.config.ts
@@ -0,0 +1,31 @@
+import { loadEnv } from '@medusajs/framework/utils'
+loadEnv('test', process.cwd())
+
+const config = {
+  transform: {
+    '^.+\\.[jt]s$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', decorators: true },
+          target: 'es2021',
+        },
+      },
+    ],
+  },
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts', 'json'],
+  modulePathIgnorePatterns: ['dist/'],
+  setupFiles: ['./integration-tests/setup.js'],
+  testMatch: [] as string[],
+}
+
+if (process.env.TEST_TYPE === 'integration:http') {
+  config.testMatch = ['**/integration-tests/http/*.spec.[jt]s']
+} else if (process.env.TEST_TYPE === 'integration:modules') {
+  config.testMatch = ['**/src/modules/*/__tests__/**/*.[jt]s']
+} else if (process.env.TEST_TYPE === 'unit') {
+  config.testMatch = ['**/src/**/__tests__/**/*.unit.spec.[jt]s']
+}
+
+export default config
