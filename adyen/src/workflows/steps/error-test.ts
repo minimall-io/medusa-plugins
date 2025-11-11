f49b6e3b0e002b5a6f8b8fa6fa201ec7bcef1a60
minimall-dev <dev@minimall.io>
2025-11-11 22:36:00 +0000

    Reformat code in error test step

--- adyen/src/workflows/steps/error-test.ts
+++ adyen/src/workflows/steps/error-test.ts
@@ -1,14 +1,10 @@
 import { MedusaError } from '@medusajs/framework/utils'
-
 import { createStep } from '@medusajs/framework/workflows-sdk'
 
 export const errorTestStepId = 'error-test-step'
 
 const errorTestStepInvoke = (message: string) => {
-  throw new MedusaError(
-    MedusaError.Types.NOT_ALLOWED,
-    message,
-  )
+  throw new MedusaError(MedusaError.Types.NOT_ALLOWED, message)
 }
 
 const errorTestStep = createStep(errorTestStepId, errorTestStepInvoke)
