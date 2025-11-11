d2b9b6ca28bb400e4df0b33ace708ff288a3ece7
minimall-dev <dev@minimall.io>
2025-11-11 17:32:56 +0000

    Make error test step accept message

--- adyen/src/workflows/steps/error-test.ts
+++ adyen/src/workflows/steps/error-test.ts
@@ -1,9 +1,14 @@
+import { MedusaError } from '@medusajs/framework/utils'
+
 import { StepResponse, createStep } from '@medusajs/framework/workflows-sdk'
 
 export const errorTestStepId = 'error-test-step'
 
-const errorTestStepInvoke = (): Promise<StepResponse<undefined, undefined>> => {
-  throw new Error('Test error')
+const errorTestStepInvoke = (message: string): Promise<StepResponse<undefined, undefined>> => {
+  throw new MedusaError(
+    MedusaError.Types.NOT_ALLOWED,
+    message,
+  )
 }
 
 const errorTestStep = createStep(errorTestStepId, errorTestStepInvoke)
