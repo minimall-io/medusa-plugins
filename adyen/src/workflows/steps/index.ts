94a1a7f157bdff6a032c30f08c918477a2865b41
minimall-dev <dev@minimall.io>
2025-11-14 23:14:06 +0000

    Export process capture failure step

--- adyen/src/workflows/steps/index.ts
+++ adyen/src/workflows/steps/index.ts
@@ -1,5 +1,6 @@
 export { default as addNotificationToPaymentDataStep } from './add-notification-to-payment-data'
 export { default as createPaymentCaptureStep } from './create-payment-capture'
 export { default as errorTestStep } from './error-test'
+export { default as processCaptureFailureStep } from './process-capture-failure'
 export { default as processCaptureSuccessStep } from './process-capture-success'
 export { default as updatePaymentStep } from './update-payment'
