fac4b57fdab4a274faca767f1db675b71f0ef16f
minimall-dev <dev@minimall.io>
2026-01-05 00:58:43 +0000

    Use default constants and refine the stored payment methods mocking

--- adyen/integration-tests/http/mocks/mockAdyenApi.ts
+++ adyen/integration-tests/http/mocks/mockAdyenApi.ts
@@ -1,5 +1,11 @@
 import nock from 'nock'
-import { HOST, SHOULD_RUN_ADYEN_API_LIVE_TESTS } from './constants'
+import {
+  DEFAULT_DELAY,
+  DEFAULT_ERROR,
+  DEFAULT_TIMES,
+  HOST,
+  SHOULD_RUN_ADYEN_API_LIVE_TESTS,
+} from './constants'
 import {
   DeleteStoredPaymentMethods,
   GetStoredPaymentMethods,
@@ -12,16 +18,23 @@ import {
 } from './responses'
 import { matchOperation, Operation } from './utils'
 
-interface IMockAdyenApi {
-  paymentAuthorisation: (times: number, delay: number) => void
-  paymentCancel: (times: number, delay: number) => void
-  paymentCapture: (times: number, delay: number) => void
-  paymentMethods: (times: number, delay: number) => void
-  paymentRefund: (times: number, delay: number) => void
-  postServerError: (times: number, delay: number, error: string) => void
+export interface IMockAdyenApi {
+  isDone: () => boolean
+  paymentAuthorisation: (times?: number, delay?: number) => void
+  paymentCancel: (times?: number, delay?: number) => void
+  paymentCapture: (times?: number, delay?: number) => void
+  paymentMethods: (times?: number, delay?: number) => void
+  paymentRefund: (times?: number, delay?: number) => void
+  postServerError: (times?: number, delay?: number, error?: string) => void
   reset: () => void
   scope: nock.Scope | null
-  storedPaymentMethods: (times: number, delay: number) => void
+  storedPaymentMethods: (
+    post?: boolean,
+    get?: boolean,
+    del?: boolean,
+    times?: number,
+    delay?: number,
+  ) => void
 }
 
 export const MockAdyenApi = (): IMockAdyenApi => {
@@ -34,30 +47,42 @@ export const MockAdyenApi = (): IMockAdyenApi => {
     scope = nock(HOST)
   }
 
-  const storedPaymentMethods = (times: number = 1, delay: number = 50) => {
+  const storedPaymentMethods = (
+    post: boolean = true,
+    get: boolean = true,
+    del: boolean = true,
+    times: number = DEFAULT_TIMES,
+    delay: number = DEFAULT_DELAY,
+  ) => {
     if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return
     const store = []
     const postStoredPaymentMethods = PostStoredPaymentMethods(store)
     const getStoredPaymentMethods = GetStoredPaymentMethods(store)
     const deleteStoredPaymentMethods = DeleteStoredPaymentMethods(store)
-    scope
-      .post(matchOperation(Operation.StoredPaymentMethods))
-      .times(times)
-      .delay(delay)
-      .reply(postStoredPaymentMethods)
-    scope
-      .get(matchOperation(Operation.StoredPaymentMethods))
-      .times(times)
-      .delay(delay)
-      .reply(getStoredPaymentMethods)
-    scope
-      .delete(matchOperation(Operation.StoredPaymentMethods))
-      .times(times)
-      .delay(delay)
-      .reply(deleteStoredPaymentMethods)
+    if (post)
+      scope
+        .post(matchOperation(Operation.StoredPaymentMethods))
+        .times(times)
+        .delay(delay)
+        .reply(postStoredPaymentMethods)
+    if (get)
+      scope
+        .get(matchOperation(Operation.StoredPaymentMethods))
+        .times(times)
+        .delay(delay)
+        .reply(getStoredPaymentMethods)
+    if (del)
+      scope
+        .delete(matchOperation(Operation.StoredPaymentMethods))
+        .times(times)
+        .delay(delay)
+        .reply(deleteStoredPaymentMethods)
   }
 
-  const paymentMethods = (times: number = 1, delay: number = 50) => {
+  const paymentMethods = (
+    times: number = DEFAULT_TIMES,
+    delay: number = DEFAULT_DELAY,
+  ) => {
     if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return
     scope
       .post(matchOperation(Operation.PaymentMethods))
@@ -66,7 +91,10 @@ export const MockAdyenApi = (): IMockAdyenApi => {
       .reply(postPaymentMethods)
   }
 
-  const paymentAuthorisation = (times: number = 1, delay: number = 50) => {
+  const paymentAuthorisation = (
+    times: number = DEFAULT_TIMES,
+    delay: number = DEFAULT_DELAY,
+  ) => {
     if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return
     scope
       .post(matchOperation(Operation.Payments))
@@ -75,7 +103,10 @@ export const MockAdyenApi = (): IMockAdyenApi => {
       .reply(postPayments)
   }
 
-  const paymentCapture = (times: number = 1, delay: number = 50) => {
+  const paymentCapture = (
+    times: number = DEFAULT_TIMES,
+    delay: number = DEFAULT_DELAY,
+  ) => {
     if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return
     scope
       .post(matchOperation(Operation.Captures))
@@ -84,7 +115,10 @@ export const MockAdyenApi = (): IMockAdyenApi => {
       .reply(postCaptures)
   }
 
-  const paymentCancel = (times: number = 1, delay: number = 50) => {
+  const paymentCancel = (
+    times: number = DEFAULT_TIMES,
+    delay: number = DEFAULT_DELAY,
+  ) => {
     if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return
     scope
       .post(matchOperation(Operation.Cancels))
@@ -93,7 +127,10 @@ export const MockAdyenApi = (): IMockAdyenApi => {
       .reply(postCancels)
   }
 
-  const paymentRefund = (times: number = 1, delay: number = 50) => {
+  const paymentRefund = (
+    times: number = DEFAULT_TIMES,
+    delay: number = DEFAULT_DELAY,
+  ) => {
     if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return
     scope
       .post(matchOperation(Operation.Refunds))
@@ -103,9 +140,9 @@ export const MockAdyenApi = (): IMockAdyenApi => {
   }
 
   const postServerError = (
-    times: number = 1,
-    delay: number = 50,
-    error: string = 'Server error',
+    times: number = DEFAULT_TIMES,
+    delay: number = DEFAULT_DELAY,
+    error: string = DEFAULT_ERROR,
   ) => {
     if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return
     scope
@@ -115,7 +152,13 @@ export const MockAdyenApi = (): IMockAdyenApi => {
       .replyWithError(error)
   }
 
+  const isDone = (): boolean => {
+    if (SHOULD_RUN_ADYEN_API_LIVE_TESTS || scope === null) return true
+    return scope.isDone()
+  }
+
   return {
+    isDone,
     paymentAuthorisation,
     paymentCancel,
     paymentCapture,
